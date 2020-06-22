b75f0f8119a6c2137efa52499f9d68e16a4d2305
Idan <idan.zonshein@powerhrg.com>
2020-06-22 11:11:09 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:password] == params[:password_confirmation]
+      @user = User.create(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+      else
+        redirect_to "/signup"
+      end
+    else
+      redirect_to "/signup"
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
